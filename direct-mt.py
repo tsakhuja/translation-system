f2f49bff199249b7b7739c0e89f3be0bee58ad67
Timothy Sakhuja <tsakhuja@gmail.com>
2014-02-20 23:20:41 -0800

    implement basic direct translation

--- direct-mt.py
+++ direct-mt.py
@@ -6,14 +6,51 @@ def load_dictionary(filename):
   d = {}
   with open(filename) as f:
     for line in f:
-       tokens = line.split()
+       tokens = line.rstrip().split(',')
        d[tokens[0]] = tokens[1:]
 
   return d
 
 
+def direct_translate(sentence, dictionary):
+  """ 
+  * sentence is a list of words or characters
+  """
+  translated_sentence = []
+  lower_bound = 0
+  length = 1
+  best_match = None
+  while lower_bound < len(sentence):
+    try:
+      print sentence[lower_bound:lower_bound + length]
+      print (lower_bound,length)
+      translated_word = dictionary[''.join(sentence[lower_bound:lower_bound + length])][0]
+    except:
+      lower_bound += 1
+      length = 1
+      if best_match:
+        translated_sentence.append(best_match)
+    else:
+      best_match = translated_word
+      if lower_bound == len(sentence) - 1:
+        translated_sentence.append(best_match)
+        break
+
+      length += 1
+
+  return translated_sentence
+
+
+
 def main():
-  print load_dictionary('data/test-dict.txt')
+  dictionary = load_dictionary('data/test-dict.txt')
+  print dictionary
+  with open('data/test-corpus.txt') as f:
+    for line in f:
+      sentence = line.split()
+      print sentence
+      print ' '.join(direct_translate(sentence, dictionary))
+
 
 if __name__ == "__main__":
     main()
