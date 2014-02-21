f3cda21c0506081667f618c895a9ac2e1ee0c4d4
Timothy Sakhuja <tsakhuja@gmail.com>
2014-02-20 22:15:16 -0800

    add support for loading dictionary from file

--- direct-mt.py
+++ direct-mt.py
@@ -1,3 +1,19 @@
-import nltk
+#import nltk
 
 #TODO Implement Chinese->English direct translation system
+
+def load_dictionary(filename):
+  d = {}
+  with open(filename) as f:
+    for line in f:
+       tokens = line.split()
+       d[tokens[0]] = tokens[1:]
+
+  return d
+
+
+def main():
+  print load_dictionary('data/test-dict.txt')
+
+if __name__ == "__main__":
+    main()
