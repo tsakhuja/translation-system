f51f5dcabcc09cedc7cd1322ad3ac1352806b5fc
Timothy Sakhuja <tsakhuja@gmail.com>
2014-02-25 09:30:39 -0800

    added pos tagging

--- direct-mt.py
+++ direct-mt.py
@@ -1,16 +1,64 @@
 import nltk
+from nltk.corpus import sinica_treebank
 from collections import defaultdict
+import pickle
 
 #TODO Implement Chinese->English direct translation system
 
+def trained_pos_tagger():
+  try:
+    with open('markov_sinica.pickle', 'r') as fd:
+      trained_pos_tagger.tagger = pickle.load(fd)
+  except:
+    if not trained_pos_tagger.tagger:
+      sents = sinica_treebank.tagged_sents(simplify_tags=True)
+      training = []
+      test = []
+      for i in range(len(sents)):
+        if i % 10:
+          training.append(sents[i])
+        else:
+          test.append(sents[i])
+      trained_pos_tagger.tagger = nltk.HiddenMarkovModelTagger.train(training)
+      print '%.1f %%' % (trained_pos_tagger.tagger.evaluate(test) * 100)
+      # Dump trained tagger
+      # with open('markov_sinica.pickle', 'w') as fd:
+      #   pickle.dump(trained_pos_tagger.tagger, fd)
+
+  return trained_pos_tagger.tagger
+
+trained_pos_tagger.tagger = None
+
+def tag_pos(sentence):
+  pos_tagger = trained_pos_tagger()
+  return pos_tagger.tag(sentence)
+
 def load_dictionary(filename):
-  d = defaultdict(list)
+  d = defaultdict(lambda: defaultdict(list))
   with open(filename) as f:
     for line in f:
-       tokens = line.rstrip().split(',')
-       d[tokens[0]].append(tokens[2])
+      tokens = line.rstrip().split(',')
+      pos = tokens[1]
+      if pos == 'noun':
+        pos = 'N'
+      elif pos == 'verb':
+        pos = 'V'
+      elif pos == 'adjective':
+        pos = 'A'
+      elif pos == 'preposition':
+        pos = 'P'
+      elif pos == 'adverb':
+        pos = 'D'
+      elif pos == 'conjunction':
+        pos = 'T'
+      elif pos == 'pronoun':
+        pos = 'N'
+      elif pos == 'auxiliary verb':
+        pos = 'V'
+      d[tokens[0]][pos].append(tokens[2])
 
   return d
+
 
 
 def direct_translate(sentence, dictionary):
@@ -18,11 +66,15 @@ def direct_translate(sentence, dictionary):
   * sentence is a list of words or characters
   """
   translated_sentence = []
-  for token in sentence:
-    try:
-      translated_word = dictionary[token][0]
-    except:
-      translated_word = token
+  for token, pos in sentence:
+    if dictionary[token].has_key(pos):
+      translated_word = dictionary[token][pos][0]
+    else:
+      # Fall back on any translation of word, even if not correct POS
+      try:
+        translated_word = dictionary[token].values()[0][0]
+      except:
+        translated_word = token
     translated_sentence.append(translated_word)
 
   return translated_sentence
@@ -35,7 +87,7 @@ def main():
     for line in f:
       sentence = line.split()
       print ' '.join(sentence)
-      print ' '.join(direct_translate(sentence, dictionary))
+      print ' '.join(direct_translate(tag_pos(sentence), dictionary))
 
 
 if __name__ == "__main__":
